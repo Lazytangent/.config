2ad4780c8b6721c02a93bf6c6ffa2f958ed3bcac
Lazytangent <pm123633@gmail.com>
2021-12-11 10:54:40 -0600

    Update custom

--- custom-emacs/custom.el
+++ custom-emacs/custom.el
@@ -92,7 +92,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(helm-ls-git evil exotica-theme use-package)))
+ '(package-selected-packages '(helm evil exotica-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
