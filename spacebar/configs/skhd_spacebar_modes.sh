9a55d2d3a5db72e17c792e9dbc80b25e2d420d38
Lazytangent <peter.tn.mai@gmail.com>
2022-03-27 11:44:47 -0500

    Start adding vim-like modes

--- spacebar/configs/skhd_spacebar_modes.sh
+++ spacebar/configs/skhd_spacebar_modes.sh
@@ -31,5 +31,11 @@ if which spacebar >/dev/null; then
       spacebar -m config center_shell           on
       spacebar -m config center_shell_command   'echo "[LAYOUT MODE]"'
       ;;
+    normal)
+      spacebar -m config background_color       0xff0ece6a
+      spacebar -m config title                  off
+      spacebar -m config center_shell           on
+      spacebar -m config center_shell_command   'echo "[NORMAL MODE]"'
+      ;;
   esac
 fi
