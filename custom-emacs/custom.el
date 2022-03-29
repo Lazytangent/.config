430ea7e7de95bd0bbbde28034888152f5218029b
Lazytangent <peter.tn.mai@gmail.com>
2022-03-28 20:12:44 -0500

    Updates

--- custom-emacs/custom.el
+++ custom-emacs/custom.el
@@ -100,8 +100,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mac-option-modifier '(:ordinary meta :function alt :mouse alt))
  '(package-selected-packages
-   '(company which-key use-package helm-ls-git exotica-theme evil)))
+   '(company which-key helm-ls-git use-package helm exotica-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
