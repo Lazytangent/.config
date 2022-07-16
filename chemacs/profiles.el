cf484b5fd823d8cacfb3feb1db73c76e76af6fac
Lazytangent <peter.tn.mai@gmail.com>
2022-07-16 16:11:38 -0500

    Add chemacs profiles

---
+++ chemacs/profiles.el
@@ -0,0 +1,4 @@
+(("default" . ((user-emacs-directory . "~/.config/doom-emacs")))
+ ("doom" . ((user-emacs-directory . "~/.config/doom-emacs")))
+ ("custom" . ((user-emacs-directory . "~/.config/custom-emacs")))
+ ("vanilla" . ((user-emacs-directory . "~/.config/vanillamacs"))))
