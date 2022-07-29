c3d4763eec28958764534976064f1c6556688294
Lazytangent <peter.tn.mai@gmail.com>
2022-07-29 18:17:55 -0500

    Set Meta key

--- plainmacs/init.el
+++ plainmacs/init.el
@@ -1,3 +1,16 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mac-option-modifier '(:ordinary meta :function alt :mouse alt)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
